753dd3df481391c3d882b99ac4c6979479ab0088
arturotorresg <arturotorresg@users.noreply.github.com>
2023-09-01 18:26:07 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <nanorpc/http/easy.h>
+#include <iostream>
+
+bool entrypoint(const std::string & str)
+{
+    return true;
+}
+
+int main()
+{
+    try
+    {
+        auto server = nanorpc::http::easy::make_server(
+            "0.0.0.0", "55555", 8, "/api/", std::pair{"setActiveDevice", entrypoint});
+
+        std::cout << "Server started. Press Enter for quit." << std::endl;
+
+        // This will effectively stop this thread from stopping, as we can not make any input
+        std::cin.get();
+    }
+    catch (std::exception const &e)
+    {
+        std::cerr << "Error: " << nanorpc::core::exception::to_string(e) << std::endl;
+        return false;
+    }
+}
